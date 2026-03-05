4985dc3e11367fcde00f005018d92190a16ff6d8
Joseph Porcelli <porcej@gmail.com>
2026-03-05 09:12:45 -0500

    Moved to Angular 18

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WeatherComponent } from './weather/weather.component';
 import { ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 
-@NgModule({
-  declarations: [AppComponent, WeatherComponent],
-  imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule, HttpClientModule],
-  providers: [],
-  bootstrap: [AppComponent],
-})
+@NgModule({ declarations: [AppComponent, WeatherComponent],
+    bootstrap: [AppComponent], imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule], providers: [provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule {}
