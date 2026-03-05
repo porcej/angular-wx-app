dd12592351196a58400377462a1e108727cbb119
Joseph Porcelli <porcej@gmail.com>
2026-03-05 09:20:16 -0500

    Moved code to latest version of Angular (21)

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -45,7 +45,7 @@
 /***************************************************************************************************
  * Zone JS is required by default for Angular itself.
  */
-import 'zone.js/dist/zone'; // Included with Angular CLI.
+import 'zone.js'; // Required for Angular (zone.js 0.14+ uses package entry).
 
 /***************************************************************************************************
  * APPLICATION IMPORTS
